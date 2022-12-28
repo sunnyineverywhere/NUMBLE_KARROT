4c810377f3f2da6d5709b5ed06aff8ede260f459
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-12-28 10:17:09 +0900

    🎉 First Commit 🎉

---
+++ src/main/java/web/karrot/CloneKarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package web.karrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneKarrotMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneKarrotMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/karrot/CloneKarrotMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package web.karrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneKarrotMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
