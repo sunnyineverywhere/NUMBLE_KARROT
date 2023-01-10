bf5f80a97a6fc14d02d94bd51e11746e294d9c69
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-01-11 01:36:21 +0900

    ✨ feature: jwt 프로바이더 추가

---
+++ src/main/java/web/karrot/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,104 @@
+package web.karrot.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import web.karrot.domain.entity.Member;
+import web.karrot.domain.repository.MemberRepository;
+
+import java.time.Duration;
+import java.util.Base64;
+import java.util.Date;
+
+/**
+ * 토큰 생성, 검증
+ * 필터 클래스에서 사전 검증을 거친다* *
+ */
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    private final MemberRepository memberRepository;
+    // private final RedisService redisService;
+
+    @Value("${spring.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    // 토큰 유효시간 30분
+    private Long accessTokenValidTime = 30 * 60 * 1000L;
+    private Long refreshTokenValidTime = 1000L * 60 * 60 * 24;
+
+    @PostConstruct
+    protected void init(){
+        SECRET_KEY = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes());
+    }
+
+    public String createToken(String value, Long tokenValidTime){
+        // JWT payload에 저장되는 정보단위. user 식별값
+        Claims claims = Jwts.claims().setSubject(value);
+
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String createAccessToken(Long memberId){
+        return this.createToken(memberRepository.findById(memberId).toString(), accessTokenValidTime);
+    }
+
+    /*
+    public String createRefreshToken(String email){
+        String refreshToken = createToken(email, refreshTokenValidTime);
+        redisService.setValues(email, refreshToken, Duration.ofMillis(refreshTokenValidTime));
+        return refreshToken;
+    }
+     */
+
+    public Authentication getAuthentication(String token){
+        Member member = (Member) memberRepository.findByEmail(getMemberInfo(token));
+        return new UsernamePasswordAuthenticationToken(member, "");
+    }
+
+    public String getMemberInfo(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveAccessToken(HttpServletRequest request){
+        return request.getHeader("Authorization");
+    }
+
+    /*
+    public String resolveRefreshToken(HttpServletRequest request){
+        return request.getHeader("refresh-token");
+    }
+     */
+
+
+    public boolean validateToken(String jwtToken){
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(jwtToken);
+            log.info(claims.toString());
+            log.info(claims.getBody().getExpiration().toString());
+            return claims.getBody().getExpiration().after(new Date());
+        }
+        catch (Exception e){
+            return false;
+        }
+
+    }
+}
--- src/main/java/web/karrot/domain/repository/MemberRepository.java
+++ src/main/java/web/karrot/domain/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package web.karrot.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import web.karrot.domain.entity.Member;
 
+import java.util.Map;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Map<Object, Object> findByEmail(String memberInfo);
 }
